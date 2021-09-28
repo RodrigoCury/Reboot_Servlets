bd11edfb43aae1a5766a37a67d86a5b53fab5095
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-28 16:12:21 -0300

    no longer accept post and get

--- gerenciador/src/main/java/br/dev/rodrigocury/gerenciador/acao/RemoveEmpresa.java
+++ gerenciador/src/main/java/br/dev/rodrigocury/gerenciador/acao/RemoveEmpresa.java
@@ -10,24 +10,6 @@ import br.dev.rodrigocury.gerenciador.models.Banco;
 
 public class RemoveEmpresa {
 	public static void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		switch (request.getMethod()) {
-			case "POST":
-				post(request, response);
-				break;
-			case "GET":
-				get(request, response);
-				break;
-			default:
-				response.sendError(405);
-		}
-	}
-
-	private static void get(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		response.sendRedirect("removeEmpresa.jsp");
-		
-	}
-
-	private static void post(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String id = (String) request.getParameter("id");
 		Integer idInt = Integer.valueOf(id);
 		
